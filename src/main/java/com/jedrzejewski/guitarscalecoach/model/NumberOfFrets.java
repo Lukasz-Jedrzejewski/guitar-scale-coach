9cb005ecba241c54780934ac4db3b53f7198ec74
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-02-01 14:39:50 +0100

    Add NumberOfFrets enum
    Create enum contains numbers of frets.

---
+++ src/main/java/com/jedrzejewski/guitarscalecoach/model/NumberOfFrets.java
@@ -0,0 +1,11 @@
+package com.jedrzejewski.guitarscalecoach.model;
+
+public enum NumberOfFrets {
+
+    nineteen(19),
+    twenty_two(22),
+    twenty_four(24),
+    ;
+
+    NumberOfFrets(int numberOfFrets) {}
+}
