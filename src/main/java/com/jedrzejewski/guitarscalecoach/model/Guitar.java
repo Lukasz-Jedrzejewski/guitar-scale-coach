6f6b911eea5aa601c99726568810cca18d49b6aa
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-02-01 15:37:36 +0100

    Add createTuning method
    Create method for defining tuning in Guitar model class.

--- src/main/java/com/jedrzejewski/guitarscalecoach/model/Guitar.java
+++ src/main/java/com/jedrzejewski/guitarscalecoach/model/Guitar.java
@@ -4,6 +4,8 @@ import com.jedrzejewski.guitarscalecoach.enumerated.NumberOfFrets;
 import com.jedrzejewski.guitarscalecoach.enumerated.NumberOfStrings;
 import com.jedrzejewski.guitarscalecoach.enumerated.Sounds;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Guitar {
@@ -11,6 +13,14 @@ public class Guitar {
     private NumberOfStrings numberOfStrings;
     private NumberOfFrets numberOfFrets;
     private Map<Integer, Sounds> tuning;
+
+    public Map<Integer, Sounds> createTuning (List<Sounds> sounds) {
+        Map<Integer, Sounds> tuning = new HashMap<>();
+        for (int i = 0; i < sounds.size(); i++) {
+            tuning.put(i+1, sounds.get(i));
+        }
+        return tuning;
+    }
 
     public NumberOfStrings getNumberOfStrings() {
         return numberOfStrings;
@@ -34,5 +44,14 @@ public class Guitar {
 
     public void setTuning(Map<Integer, Sounds> tuning) {
         this.tuning = tuning;
+    }
+
+    @Override
+    public String toString() {
+        return "Guitar{" +
+                "numberOfStrings=" + numberOfStrings +
+                ", numberOfFrets=" + numberOfFrets +
+                ", tuning=" + tuning +
+                '}';
     }
 }
