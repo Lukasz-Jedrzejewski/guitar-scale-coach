7affbdae474e202d33472fb0418cd21c57044aea
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2021-02-01 14:22:51 +0100

    Add NumberOfStrings enum
    Create enum contains numbers of strings.
    Transfer controller package to right place.

---
+++ src/main/java/com/jedrzejewski/guitarscalecoach/model/NumberOfStrings.java
@@ -0,0 +1,14 @@
+package com.jedrzejewski.guitarscalecoach.model;
+
+public enum NumberOfStrings {
+
+    four(4),
+    five(5),
+    six(6),
+    seven(7),
+    eight(8),
+    ten(10),
+    twelve(12);
+
+    NumberOfStrings(int numberOfStrings) {}
+}
